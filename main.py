aaf9c9d534b61c1ece989b66cc8136072b658ffe
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-06-29 18:40:37 +0300

    tracker.py bug fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
   video_frames = read_video("input_videos/08fd33_4.mp4")
   #initialize tracker
   tracker = Tracker('models/best.pt')
-  tracks = tracker.get_object_tracks(video_frames)
+  tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stub/track_stubs.pkl')
   
   
   #Save the video
