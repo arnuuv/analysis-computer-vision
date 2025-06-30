93172b3462e3b4f4767b4194a3e9288bbc5da149
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-06-30 14:47:50 +0300

    drawing annotations for characters

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -4,7 +4,7 @@ import os
 import pickle
 import cv2
 from analysis_computer_vision.utils import get_center_of_bbox, get_bbox_width
-
+import numpy as np
 class Tracker:
     def __init__(self, model_path):
         self.model = YOLO(model_path)
@@ -71,7 +71,7 @@ class Tracker:
                 pickle.dump(tracks, f)
         return tracks
     
-    def draw_ellipse(self,frame,bbox,color,track_id):
+    def draw_ellipse(self,frame,bbox,color,track_id=None):
         y2 = int(bbox[3])
         x_center, _ = get_center_of_bbox(bbox)
         width = get_bbox_width(bbox)
@@ -86,24 +86,56 @@ class Tracker:
             thickness=2,
             lineType=cv2.LINE_4
         )
+        #draw rectangle
+        rectangle_width = 40
+        rectangle_height = 20
+        x1_rect =x_center - rectangle_width/2
+        x2_rect = x_center + rectangle_width/2
+        y1_rect = (y2-rectangle_height//2) + 15
+        y2_rect = (y2+rectangle_height//2) + 15
+        
+        if track_id is not None:
+            cv2.rectangle(frame, (int(x1_rect), int(y1_rect)), (int(x2_rect), int(y2_rect)), color, cv2.FILLED)
+            
+            x1_text =x1_rect + 12
+            if track_id>99: 
+                x1_text -= 10
+            cv2.putText(frame, f"{track_id}", (int(x1_text), int(y1_rect)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,0), 2)
         return frame
     
-    
-    def draw_annotations(self,video_frames,tracks):
+    def draw_triangle(self,frame,bbox,color):
+        y =int(bbox[1])
+        x,_=get_center_of_bbox(bbox)
+        triangle_points = np.array([
+            [x,y],
+            [x-10,y-20],
+            [x+10,y-20]
+        ])
+        cv2.drawContours(frame, [triangle_points], 0, color, cv2.FILLED)
+        cv2.drawContours(frame, [triangle_points], 0, (0,0,0), 2)
+        
+        return frame
+        
+    def draw_annotations(self, video_frames, tracks):
         output_video_frames = [] 
         for frame_num, frame in enumerate(video_frames):
             frame = frame.copy()
             
+            # Draw players (red ellipse with track_id)
             player_dict = tracks["players"][frame_num]
-            # Only draw red circles for players
             for track_id, player in player_dict.items():
                 frame = self.draw_ellipse(frame, player["bbox"], (0,0,255), track_id)
             
+            # Draw referees (yellow ellipse with track_id)
             referee_dict = tracks["referees"][frame_num]
             for track_id, referee in referee_dict.items():
                 frame = self.draw_ellipse(frame, referee["bbox"], (0,255,255), track_id)
             
+            # Draw ball (green ellipse, no track_id)
+            ball_dict = tracks["ball"][frame_num]
+            for track_id, ball in ball_dict.items():
+                frame = self.draw_triangle(frame, ball["bbox"], (0,255,0))
             
             output_video_frames.append(frame)
-            
+        
         return output_video_frames
