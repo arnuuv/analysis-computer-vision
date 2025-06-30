e6faae23ad299534d8a630cb816e3bdf47ef35a6
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-06-30 15:30:39 +0300

    clustering for player bounding box

---
+++ team_assigner/team_assigner.py
@@ -0,0 +1,28 @@
+from sklearn.cluster import KMeans
+
+class TeamAssigner:
+    def __init__(self):
+        pass
+    
+    def get_clustering_model(self,image):
+      image_2d =image.reshape(-1,3)
+      kmeans = KMeans(n_clusters=2,init="k-means++",n_init=1)
+      kmeans.fit(image_2d)
+      return kmeans
+      
+      
+      
+    def get_player_color(self,frame,bbox):
+      image = frame[int(bbox[1]):int(bbox[3]),int(bbox[0]):int(bbox[2])]
+      
+      top_half_image = image[0:int(image.shape[0]/2),:]
+      
+      #get clustering model
+      kmeans =self.get_clustering_model(top_half_image)
+
+    def assign_team_color(self,frame,player_detections):
+        player_color=[]
+        for _,player_detection in player_detections.items():
+          bbox = player_detection["bbox"]
+          player_color =self.get_player_color(frame,bbox)
+          
