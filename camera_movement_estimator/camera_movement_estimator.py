4b5766d8a3b9d46f79bf6405fdfb6aafb0140269
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-07-02 16:00:37 +0300

    constant motion tracking

---
+++ camera_movement_estimator/camera_movement_estimator.py
@@ -0,0 +1,23 @@
+import pickle
+import cv2
+import numpy as np
+class CameraMovementEstimator():
+  def __init__(self,frame):
+    first_frame_grayscale = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    mask_features = np.zeros_like(first_frame_grayscale)
+    mask_features[:,0:20] = 1
+    mask_features[:,900:1050]=1
+    self.features = dict(
+      maxCorners = 100,
+      qualitylevel =0.3,
+      minDistance = 3,
+      blockSize = 7,
+      mask = mask_features,
+      
+    )
+    
+  def get_camera_movement(self,frames,read_from_stub=False, stub_path=None):
+    #read the stub
+    camera_movement = [[0,0]*len(frames)]
+    old_gray = cv2.cvtColor(frames[0],cv2.COLOR_BGR2GRAY)
+    old_features = cv2.goodFeaturesToTrack(old_gray,)
--- team_assigner/team_assigner.py
+++ team_assigner/team_assigner.py
@@ -44,7 +44,7 @@ class TeamAssigner:
           player_color =self.get_player_color(frame,bbox)
           player_colors.append(player_color)
           
-        kmeans = KMeans(n_clusters=2,init="k-means++",n_init=1)
+        kmeans = KMeans(n_clusters=2,init="k-means++",n_init=10)
         kmeans.fit(player_colors)
         self.kmeans = kmeans
         self.team_colors[1] = kmeans.cluster_centers_[0]
