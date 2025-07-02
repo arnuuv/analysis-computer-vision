6b4a6c4ccc4587b122b71aa978fb442bb22632e9
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-07-02 20:48:21 +0300

    transformed point reshaping

--- view_transformer/view_transformer.py
+++ view_transformer/view_transformer.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import cv2
 class view_transformer():
   def __init__(self,frame):
     court_width = 68 
@@ -21,3 +22,28 @@ class view_transformer():
     self.pixel_vertices = self.target_vertices.astype(np.float32)
     
     self.perspective_transform = cv2.getPerspectiveTransform(self.pixel_vertices,self.target_vertices)
+    
+def transform_point(self,point):
+  p = int(point[0]), int(point[1])
+  is_inside = cv2.pointPolygonTest(self.pixel_vertices,p,False)>=0
+  if not is_inside:
+    return None
+  reshaped_point = point.reshape(-1,1,2)
+  
+  
+  
+  
+def add_transformed_position_to_tracks(self,tracks):
+  for object,object_tracks in tracks.items():
+    for frame_num,track in enumerate(object_tracks):
+      for track_id,track_info in track.items():
+        position = track_info['position_adjusted']
+        position =np.array(position)
+        position_transformed = self.transform_point(position)
+        if position_transformed is not None:
+          position_transformed = position_transformed.squeeze().tolist()
+          tracks[object][frame_num][track_id]['position_transformed'] = position_transformed
+          
+          
+        
+
