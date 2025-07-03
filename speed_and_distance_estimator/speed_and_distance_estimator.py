44935d52c1f0ebf48c3909e67813503e26b84f7f
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-07-03 11:24:12 +0300

    speed and distance estimator

---
+++ speed_and_distance_estimator/speed_and_distance_estimator.py
@@ -0,0 +1,54 @@
+
+
+import sys
+sys.path.append('../')
+from utils import measure_distance 
+
+
+class SpeedAndDistance_Estimator():
+  def __init__(self):
+    self.frame_window = 5
+    self.frame_rate = 24
+    
+    
+  def add_speed_and_distance_to_tracks(self,tracks):
+    total_distance ={
+      
+    }
+    for object, object_tracks in tracks.items():
+      if object == 'ball' or object == 'referee':
+        continue
+      number_of_frames = len(object_tracks)
+      for frame_num in range(0,number_of_frames, self.frame_window):
+        last_frame = min(frame_num + self.frame_window, number_of_frames)
+        
+        for track_id in object_tracks[frame_num].items:
+          if track_id not in object_tracks[last_frame]:
+            continue
+          start_position = object_tracks[frame_num][track_id]['position_transformed']
+          end_position = object_tracks[last_frame][track_id]['position_transformed']
+          
+          if start_position is None or end_position is None:
+            continue
+          
+          distance_covered = measure_distance(start_position, end_position)
+          time_elapsed = (last_frame - frame_num) / self.frame_rate
+          speed_meters_per_second = distance_covered / time_elapsed
+          speed_km_per_hour = speed_meters_per_second * 3.6
+          
+          if object not in total_distance:
+            total_distance[object] = {}
+            
+          if track_id not in total_distance[object]:
+            total_distance[object][track_id] = 0
+          
+          total_distance[object][track_id] += distance_covered
+          
+          
+          for frame_num_batch in range(frame_num,last_frame):      
+    
+  
+  
+  
+  
+  
